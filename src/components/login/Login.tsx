1025a3d351b0cf937de078742bc0be48503638e4
Alexandru Pop <alex99_pop@yahoo.com>
2024-04-16 14:38:05 +0300

    add useLoginForm hook

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,23 +1,27 @@
+import { log } from "console";
+import useLoginForm from "../../hooks/useLoginForm";
 import DoughnutBankIcon from "../general/DoughnutBankIcon";
 
 const Login = () => {
+  const { loginForm, handleChange } = useLoginForm();
+
   return (
     <section className=" text-center text-lg-start">
       <div className="card mb-3">
         <div className="row g-0 d-flex align-items-center">
-          <div className="col-lg-4 d-none d-lg-flex">
+          <div className="col-lg-8 align-items-center justify-content-center background-color-black">
             <DoughnutBankIcon></DoughnutBankIcon>
-          </div>
-          <div className="col-lg-8">
             <div className="card-body py-5 px-md-5">
-              <form>
+              <form className="was-validated">
                 <div data-mdb-input-init className="form-outline mb-4">
                   <input
                     type="email"
-                    id="form2Example1"
+                    id="emailId"
                     className="form-control"
+                    value={loginForm.email}
+                    onChange={handleChange}
                   />
-                  <label className="form-label" htmlFor="form2Example1">
+                  <label className="form-label" htmlFor="emailId">
                     Email address
                   </label>
                 </div>
@@ -25,41 +29,21 @@ const Login = () => {
                 <div data-mdb-input-init className="form-outline mb-4">
                   <input
                     type="password"
-                    id="form2Example2"
+                    id="passwordId"
                     className="form-control"
+                    value={loginForm.password}
+                    onChange={handleChange}
                   />
-                  <label className="form-label" htmlFor="form2Example2">
+                  <label className="form-label" htmlFor="passwordId">
                     Password
                   </label>
                 </div>
 
-                <div className="row mb-4">
-                  <div className="col d-flex justify-content-center">
-                    <div className="form-check">
-                      <input
-                        className="form-check-input"
-                        type="checkbox"
-                        value=""
-                        id="form2Example31"
-                        checked
-                      />
-                      <label
-                        className="form-check-label"
-                        htmlFor="form2Example31"
-                      >
-                        {" "}
-                        Remember me{" "}
-                      </label>
-                    </div>
-                  </div>
-
-                  <div className="col">
-                    <a href="#!">Forgot password?</a>
-                  </div>
-                </div>
-
                 <button
                   type="button"
+                  onClick={() =>
+                    console.log("LoginForm: " + JSON.stringify(loginForm))
+                  }
                   data-mdb-button-init
                   data-mdb-ripple-init
                   className="btn btn-primary btn-block mb-4"
