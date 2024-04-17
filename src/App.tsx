4000cd3492a00d69c3f736d4b0f75531bd268779
Alexandru Pop <alex99_pop@yahoo.com>
2024-04-17 17:36:52 +0300

    run lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import React from "react";
-import "./App.css";
-import Login from "./components/login/Login";
-import { ToastMessage } from "./components/general/ToastMessage";
-import OrderCar from "./components/transactions/OrderCar";
+import React from 'react';
+import './App.css';
+import Login from './components/login/Login';
+import { ToastMessage } from './components/general/ToastMessage';
+import OrderCar from './components/transactions/OrderCar';
 
 function App() {
   return (
     <div className="App">
       <Login></Login>
       <ToastMessage />
-      {/* <OrderCar></OrderCar> */}
-    </div>  
+      <OrderCar></OrderCar>
+    </div>
   );
 }
 
